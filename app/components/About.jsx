de317cc762ec311657d87fd1bef84d9979a4d6a0
Jacob Werderits <werderitsj@gmail.com>
2016-10-05 19:57:55 -0400

    fix about page update

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,8 +2,11 @@ var React = require("react");
 
 var About = (props) => {
   return(
-    <h3>About</h3>
-    <p>This is the About Page</p>
+    <div>
+      <h3>About</h3>
+      <p>This is the About Page</p>
+    </div>
+
   )
 };
 
