c914dfa5944416f6faa2a0bb7228c28cbe88fdd8
Jacob Werderits <werderitsj@gmail.com>
2016-10-10 21:00:06 -0400

    style about page and links

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,9 +3,19 @@ var React = require("react");
 var About = (props) => {
   return(
     <div>
-      <h3>About</h3>
-      <p>This is the About Page</p>
-      <p>More about content</p>
+      <h1 className='text-center'>About</h1>
+      <p>Do you like Weather? Boy, is this the app for you!</p>
+      <p>Here are some of the tools I've used:</p>
+      <ul>
+        <li>
+          <a href='https://facebook.github.io/react'>React</a> - This was
+            the JavaScript framework used.
+        </li>
+        <li>
+          <a href='http://openweathermap.org'>Open Weather Map</a> - Open
+            Weather Map provides the data by city name.
+        </li>
+      </ul>
     </div>
 
   )
